0c14e40bcf3c13341ce8bafd3524831b1736873b
Tyson Andre <tysonandre775@hotmail.com>
2017-05-07 23:18:31 -0700

    Only evaluate right hand side of `??` if left value is null
    It's possible to optimize the special cases.
    `reset()` wasn't used because that would emit a warning in php7 if
    non-references were used.
    
    Motivation: if the right hand size can throw,
    then the transpiled code should only evaluate the right hand side
    if the left hand side is `null`.
    (E.g. `foo($args) ?? bar($otherargs)`, `bar` may throw)
    
    E.g. this converts `$x = $y ?? 2` into the below expression
    (`?:` is the only operator which evaluates the left hand side once
    and conditionally evaluates the right hand side)
    
    $x = (function ($v1) {
        return $v1[0];
    })(\call_user_func(function ($v1) {
            return isset($v1) ? array($v1) : null;
    }, @$y) ?: array(2));

--- lib/Transphpile/Transpile/Visitors/Php70/NullCoalesceVisitor.php
+++ lib/Transphpile/Transpile/Visitors/Php70/NullCoalesceVisitor.php
@@ -10,18 +10,28 @@ use PhpParser\NodeVisitorAbstract;
  * Converts $a ?? $b into:
  *
  *      call_user_func(
- *          function ($v1, $v2) { return isset($v1) ? $v1 : $v2; },
- *          @$a,
- *          @$b
+ *          function($v1) { return $v1[0]; },
+ *          call_user_func(
+ *              function($v1) { return $v1 !== null ? array($v1) : null; },
+ *              @$a
+ *          ) ?: [$b]
  *      )
  *
- * This construct is needed because isset() only works on variables, while the null coalesce supports any expression.
+ * (Can't use (expr)[0] in php 5.6)
+ * (Can't use reset(expr) in php 7.0+ without a warning about taking a reference on non-reference)
+ *
+ * This construct is sometimes needed because isset() only works on variables, while the null coalesce supports any expression.
  * We also add a @ operator to the variable, in case it doesn't exist so it does not throw a notice.
  *
+ * TODO: Can optimize/simplify the cases where the left hand side is always simple,
+ * and avoid method calls (Assume ArrayAccess is implemented properly).
+ *
+ * - e.g. convert `$x->y['key'] ?? DEFAULT` into `isset($x->y['key']) ? $x->y['key'] : DEFAULT`
  */
 
 class NullCoalesceVisitor extends NodeVisitorAbstract
 {
+
     public function leaveNode(Node $node)
     {
         if (!$node instanceof Node\Expr\BinaryOp\Coalesce) {
@@ -29,10 +39,9 @@ class NullCoalesceVisitor extends NodeVisitorAbstract
         }
 
         // Create closure node
-        $closureNode = new Node\Expr\Closure(array(
+        $closureLHSNode = new Node\Expr\Closure(array(
             'params' => array(
                 new Node\Param('v1'),
-                new Node\Param('v2'),
             ),
             'stmts' => array(
                 new Node\Stmt\Return_(
@@ -40,23 +49,61 @@ class NullCoalesceVisitor extends NodeVisitorAbstract
                         new Node\Expr\Isset_(array(
                             new Node\Expr\Variable('v1'),
                         )),
-                        new Node\Expr\Variable('v1'),
-                        new Node\Expr\Variable('v2')
+                        new Node\Expr\Array_([
+                            new Node\Expr\ArrayItem(
+                                new Node\Expr\Variable('v1')
+                            ),
+                        ]),
+                        new Node\Expr\ConstFetch(new Node\Name('null'))
                     )
                 )
             )
         ));
 
         // Create call_user_func() call
-        $callUserFuncNode = new Node\Expr\FuncCall(
-            new Node\Name('call_user_func'),
+        // Implementation of coalesce LHS
+        $coalesceLhs = new Node\Expr\FuncCall(
+            new Node\Name\FullyQualified('call_user_func'),
             array(
-                $closureNode,
+                $closureLHSNode,
                 new Node\Expr\ErrorSuppress($node->left),
-                new Node\Expr\ErrorSuppress($node->right),
             )
         );
 
-        return $callUserFuncNode;
+        $inner = new Node\Expr\Ternary($coalesceLhs, null, new Node\Expr\Array_([
+            new Node\Expr\ArrayItem(
+                $node->right
+            ),
+        ]));
+
+        return $this->buildGetFirstElement($inner);
+    }
+
+    /**
+     * @return Node PHP code to fetch the first element
+     */
+    private function buildGetFirstElement(Node $node) {
+        // This closure does the same thing as `new Node\Name\FullyQualified('reset')`
+        // but avoids the notice about taking a reference of a non-reference in php7.0+
+        $closureLHSNode = new Node\Expr\Closure(array(
+            'params' => array(
+                new Node\Param('v1'),
+            ),
+            'stmts' => array(
+                new Node\Stmt\Return_(
+                    new Node\Expr\ArrayDimFetch(
+                        new Node\Expr\Variable('v1'),
+                        new Node\Scalar\LNumber(0)
+                    )
+                )
+            )
+        ));
+        return new Node\Expr\FuncCall(
+            new Node\Name\FullyQualified('call_user_func'),
+            array(
+                $closureLHSNode,
+                $node,
+            )
+        );
     }
 }
